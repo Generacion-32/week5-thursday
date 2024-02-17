842f50270a2221a161ba798ceeb1d2f6e320023c
Gabriel Martinez <martinezgabrielalejandro4@gmail.com>
2024-02-16 21:01:11 -0300

    advances

--- src/tests/song.test.js
+++ src/tests/song.test.js
@@ -39,10 +39,47 @@ test("Post -> 'URL_SONGS', should return status code 201, res.body to be defined
 
 
 //get all
+test("GET -> 'URL_SONGS', should return status code 200 and res.body.length === 1", async () => {
+  const res = await request(app)
+    .get(URL_SONGS)
+
+  expect(res.status).toBe(200)
+  expect(res.body).toBeDefined()
+  expect(res.body).toHaveLength(1)
+
+  expect(res.body[0].artists).toBeDefined()
+  expect(res.body[0].artists).toHaveLength(0)
+
+  expect(res.body[0].genres).toBeDefined()
+  expect(res.body[0].genres).toHaveLength(0)
+
+})
 
 //get one
+test("GET ONE -> 'URL_SONGS/:id', should return status code 200 and res.body.name === song.name", async () => {
+  const res = await request(app)
+    .get(`${URL_SONGS}/${songId}`)
+
+  expect(res.status).toBe(200)
+  expect(res.body).toBeDefined()
+  expect(res.body.name).toBe(song.name)
+})
 
 //put
+test("PUT -> 'URL_SONGS/:id', should return status code 200 and res.body.name === songUpdate.name", async () => {
+
+  const songUpdate = {
+    name: "give me a kiss"
+  }
+
+  const res = await request(app)
+    .put(`${URL_SONGS}/${songId}`)
+    .send(songUpdate)
+
+  expect(res.status).toBe(200)
+  expect(res.body).toBeDefined()
+  expect(res.body.name).toBe(songUpdate.name)
+})
 
 test("Delte 'URL_SONGS', should return status code 204", async () => {
   const res = await request(app)
